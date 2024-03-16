48d7d917da194c39c35a2b5da07ee6ef187bb690
AnyumbaKE <esanyumba@gmail.com>
2024-03-16 22:30:12 +0300

    update

--- jabali/jabali/urls.py
+++ jabali/jabali/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from jabaliApp import views
+from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
--- jabali/jabaliApp/admin.py
+++ jabali/jabaliApp/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from .models import Customer,Product,Orders,Feedback
 # Register your models here.
